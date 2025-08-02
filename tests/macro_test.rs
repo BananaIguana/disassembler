7e4185f791061f81f9f93ab01cb41e5e8ff40a06
Ian Sidor <ian@bananaiguana.co.uk>
2025-08-02 04:00:54 +0100

    Testing LSL in preparation for instruction rendering

--- tests/macro_test.rs
+++ tests/macro_test.rs
@@ -15,7 +15,9 @@ pub fn macro_test() -> Result<(), Report>
     // let instr = 2432730111; // add sp, sp, #0x20
     // let instr = 2432713725; // add x29, sp, #0x10
     // let instr = 2432697321; // add x9, sp, #0x0
-    let instr = 2436763912; // add x8, x8, #0xF84
+
+    // let instr = 0x913E1108; // add x8, x8, #0xF84
+    let instr = 0x917E1108; // add x8, x8, #0xF84 (contains LSL)
     let decoded = ADD_ALT::from(instr);
 
     println!("{:?}", decoded);
